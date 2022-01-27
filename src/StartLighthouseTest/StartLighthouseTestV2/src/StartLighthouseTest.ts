7b63225ea61fba25256f0727d98d900d1e9d4f00
Ove Lartelius <ove_lartelius@hotmail.com>
2022-01-27 12:17:53 +0100

    :memo: Added ts scripts

---
+++ src/StartLighthouseTest/StartLighthouseTestV2/src/StartLighthouseTest.ts
@@ -0,0 +1,54 @@
+import tl = require("azure-pipelines-task-lib/task");
+import { basename } from "path";
+
+import {
+    logInfo,
+    logError
+}  from "./agentSpecific";
+
+export async function run() {
+    try {
+        // Get the build and release details
+        let EnvironmentId = tl.getInput("EnvironmentId");
+        let Commit = tl.getInput("Commit");
+
+        // we need to get the verbose flag passed in as script flag
+        var verbose = (tl.getVariable("System.Debug") === "true");
+
+        // find the executeable
+        let executable = "pwsh";
+        if (tl.getVariable("AGENT.OS") === "Windows_NT") {
+            if (!tl.getBoolInput("usePSCore")) {
+                executable = "powershell.exe";
+            }
+            logInfo(`Using executable '${executable}'`);
+        } else {
+            logInfo(`Using executable '${executable}' as only only option on '${tl.getVariable("AGENT.OS")}'`);
+        }
+
+        // we need to not pass the null param
+        var args = [__dirname + "\\StartLighthouseTest.ps1",
+        "-EnvironmentId", EnvironmentId,
+        "-Commit", Commit
+        ];
+
+        logInfo(`${executable} ${args.join(" ")}`);
+
+        var spawn = require("child_process").spawn, child;
+        child = spawn(executable, args);
+        child.stdout.on("data", function (data) {
+            logInfo(data.toString());
+        });
+        child.stderr.on("data", function (data) {
+            logError(data.toString());
+        });
+        child.on("exit", function () {
+            logInfo("Script finished");
+        });
+    }
+    catch (err) {
+        logError(err);
+    }
+}
+
+run();
